913f9f059c64a16aa712d30d068bf968fa3b4e97
Stefi <114958335+ssnakeoil@users.noreply.github.com>
2022-11-25 22:08:47 -0500

    establishing a pool of characters for generator to pick from

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,12 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Characters for generator to pick from
+var letterCapital = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var letterLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialChar = [" ", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", "<", ">", "/", "?", ",", ".", "[", "]", "{", "}", "|", "~", "`"];
+var count = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
