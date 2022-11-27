3309e164e3687d4952f3ac93983417e80bd24f7b
Stefi <114958335+ssnakeoil@users.noreply.github.com>
2022-11-27 17:36:57 -0500

    adding character concats and for loop to add characters

--- Develop/script.js
+++ Develop/script.js
@@ -14,12 +14,12 @@ var count = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 // Prompt to customize password length
 function generateLength() {
   let passwordLength = prompt("How long would you like your password to be?");
-  if (typeof passwordLength === "string") {
+  if (typeof passwordLength === String) {
     alert("Value entered is not a number. Please choose a number between 4 and 20.");
     generateLength();
   }
-  else if (passwordLength <= 4 || passwordLenght >= 20) {
-    alert("Number is not between 4 and 20.");
+  else if (passwordLength <= 4 || passwordLength >= 21) {
+    alert("Number is not between 5 and 20.");
     generateLength();
   }
   else {
@@ -34,13 +34,35 @@ function generatePassword () {
   let settingNorm = confirm("Confirm for special characters: !@#$%^&*()=-<>/?,.");
   let settingWeird = confirm("Confirm for spaces and special characters: []{}|~`_+");
   let settingCount = confirm("Confirm for numbers.");
-
+  
+  // User receives error if no parameters are chosen
   if (!settingCapital && !settingLower && !settingNorm && !settingWeird && !settingCount) {
-    alert("Password cannot be generated with no parameters.")
+    alert("Please select one.")
     generatePassword();
   }
-  var userPassword = "";
 
+  // Creating an empty variable to concat onto
+  var userPassword = [];
+
+  // Establishing concat action if statements for generator to loop through depending on chosen settings
+  if (settingCapital) {userPassword.concat(letterCapital)}
+  if (settingLower) {userPassword.concat(letterLower)}
+  if (settingNorm) {userPassword.concat(specialNorm)}
+  if (settingWeird) {userPassword.concat(specialWeird)}
+  if (settingCount) {userPassword.concat(count)}
+
+  // Temporary console.log to see if generator is working correctly at this point. (REMOVE)
+  console.log(userPassword)
+
+  // Creating an empty variable for generator to fill into
+  var passwordPrintable = ""
+
+  // Adding for loop that will loop through if statements for as many characters as the user selected
+  for (var i = 0; i < generateLength; i++) {
+    passwordPrintable = passwordPrintable + userPassword[Math.floor(Math.random()*userPassword.length)]
+  }
+
+  //
 
 }
 
